50260ccc9e1e0b5c821c4500bb331390bde12a2b
Naveenkumar-Dev-21 <naveenkumar03585@gmail.com>
2025-12-17 09:31:38 +0530

    fix: Changed components to Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LoginPage from './components/LoginPage'
-import Signup from './components/Signup'
+import LoginPage from './Pages/LoginPage'
+import Signup from './Pages/Signup'
 function App() {
   return (
     <Router>
